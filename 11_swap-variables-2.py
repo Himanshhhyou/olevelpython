ae6a9e57b99fade4ab57a06795c41567d0e91561
Himanshu Kushwaha <86154077+Himanshhhyou@users.noreply.github.com>
2025-09-16 21:52:55 +0530

    Add files via upload

---
+++ 11_swap-variables-2.py
@@ -0,0 +1,13 @@
+#swap variables method 2
+
+a = 8
+b = 9
+print("Before swap: ")
+print("a: ",a)
+print("b: ",b)
+#a,b = b,a
+a = b
+b = a
+print("After swap: ")
+print("a: ",a)
+print("b: ",b)
---
+++ 12_swap-variables-3.py
@@ -0,0 +1,13 @@
+#swap variable method 3
+a = 9
+b = 3
+print("Before swap: ")
+print("a: ",a)
+print("b: ",b)
+c = a+b #12
+b = c-b  #9
+a = c-a #3
+
+print("After swap: ")
+print("a: ",a)
+print("b: ",b)
