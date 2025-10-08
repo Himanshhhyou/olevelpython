9302aadae67a7936cfd7a11850f03432b53b4d99
Himanshu Kushwaha <86154077+Himanshhhyou@users.noreply.github.com>
2025-10-08 13:37:43 +0530

    Update 23_calendar.py

--- 23_calendar.py
+++ 23_calendar.py
@@ -8,23 +8,3 @@ print(calendar.month(2025,10))
 
 #leap year check
 print(calendar.isleap(2020))
-
-#0 - mon , 1 tuesday 2- wednesday,..................
-d = calendar.weekday(2025,10,12))
-
-if d == 0:
-    print("Monday")
-elif d == 1:
-    print("Tuesday")
-elif d == 2:
-    print("Wednesday")
-elif d == 3:
-    print("Thursday")
-elif d == 4:
-    print("Friday")
-elif d == 5:
-    print("Saturday")
-else:
-    print("Sunday")
-
-    print("Invalid day")
